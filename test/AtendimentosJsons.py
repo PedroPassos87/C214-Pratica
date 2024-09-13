bba283f73aca431dd983cead3ec757dc1ad24f27
Alves <gfernandes@ges.inatel.br>
2024-09-13 20:08:17 -0300

    Pasta test

---
+++ test/AtendimentosJsons.py
@@ -0,0 +1,27 @@
+class HorariosJsons:
+    Francisco = (
+        "{ \"id\": \"1\", "
+        "\"nomeDoProfessor\": \"Francisco\", "
+        "\"horarioDeAtendimento\": \"09:00 - 11:00\", "
+        "\"periodo\": \"integral\", "
+        "\"sala\": \"3\", "
+        "\"predio\": \"1\" }"
+    ) 
+
+    Chris = (
+        "{ \"id\": \"2\", "
+        "\"nomeDoProfessor\": \"Chris\", "
+        "\"horarioDeAtendimento\": \"14:00 - 16:00\", "
+        "\"periodo\": \"noturno\", "
+        "\"sala\": \"8\", "
+        "\"predio\": \"2\" }"
+    )
+
+    Marcelo = (
+        "{ \"id\": \"3\", "
+        "\"nomeDoProfessor\": \"Marcelo\", "
+        "\"horarioDeAtendimento\": \"10:00 - 12:00\", "
+        "\"periodo\": \"integral\", "
+        "\"sala\": \"12\", "
+        "\"predio\": \"3\" }"
+    )
