f1ffd593fbde2612bdf4158500a72fba409a8d6a
Alves <gfernandes@ges.inatel.br>
2024-10-01 22:27:18 -0300

    peter_steps

--- scripts/notification.sh
+++ scripts/notification.sh
@@ -1,5 +1,7 @@
+import os
+
 pwd
 false
 echo "HAHAHAHAHAHAHAH"
 sudo apt-get install mailutils
-echo "Pipeline executada!" | mail -s "GithubActions" ${{ secrets.EMAIL_NOTIFICACAO }}
+echo "Pipeline executada!" | mail -s "GithubActions" ${ os.getven(EMAIL_NOTIFICACAO) }
