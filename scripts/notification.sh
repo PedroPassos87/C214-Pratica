a1188e69df7eebd0399d4c3413f41148d6c883c6
Alves <gfernandes@ges.inatel.br>
2024-10-01 23:02:47 -0300

    a2

--- scripts/notification.sh
+++ scripts/notification.sh
@@ -1,2 +1,11 @@
-sudo apt-get install mailutils
-echo "Pipeline executada!" | mail -s "Github Actions" ${secrets.EMAIL_NOTIFICACAO}
+#!/bin/bash
+
+RECIPIENT="$EMAIL_NOTIFICACAO"
+
+if [ -z "$RECIPIENT" ]; then
+  echo "A variável de ambiente EMAIL_NOTIFICACAO não está definida."
+  exit 1
+fi
+
+
+echo "Pipeline executado!" | mail -s "Github Actions" "$RECIPIENT"
