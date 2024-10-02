ba3ddc627d75269d24cfdd7a994781231ade44b2
PedroPassos87 <pedro.passos@inatel.br>
2024-10-01 22:34:58 -0300

    tenta arrumar ai, meu capitao

--- scripts/notification.py
+++
@@ -1,9 +0,0 @@
-import win32com.client as win32
-
-# Cria uma instância do Outlook
-outlook = win32.Dispatch('Outlook.Application')
-message = outlook.CreateItem(0)  # Cria um novo item de mensagem
-message.To = 'EMAIL_NOTIFICACAO'  # Aqui você precisa usar a variável de ambiente
-message.Subject = 'GitHub Actions'
-message.Body = 'Pipeline executado!'
-message.Send()
